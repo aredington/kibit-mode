c0a1d79e30b8e2cb9d0ea3b3d83ca00278a3966d
William Roe <willroe@gmail.com>
2014-08-18 10:58:19 +0100

    Bump dep on kibit to 0.0.8 and clojure to 1.6.0
    There have been some additions that would be super useful for those
    using kibit-mode to define a flycheck checker for their Clojure.
    
    Also, honorable mention to current versions of Midje and lein-midje

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject kibit-mode "0.0.1"
   :description "A kibit compiler for Clojure files"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [jonase/kibit "0.0.3"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [jonase/kibit "0.0.8"]]
   :profiles {:dev {:resource-paths ["bogus_src"]
-                   :dependencies [[midje "1.4.0"]
-                                  [lein-midje "2.0.0-SNAPSHOT"]]}}
-  :eval-in :leiningen
-  )
+                   :dependencies [[midje "1.6.3"]
+                                  [lein-midje "3.1.3"]]}}
+  :eval-in :leiningen)
